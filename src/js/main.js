6b934217029aac630171e0b55284fc5f92d06b1e
Mielilon <daria.mg354@mail.ru>
2020-11-13 17:02:39 +0300

    Сделан смах

--- src/js/main.js
+++ src/js/main.js
@@ -20,3 +20,37 @@ setInterval(function() {
     $(`input[type='radio']`)[intervalCounter - 1].checked = true;
 }, 5000);
 
+
+
+var initialPoint;
+var finalPoint;
+document.addEventListener('touchstart', function(event) {
+event.preventDefault();
+event.stopPropagation();
+initialPoint=event.changedTouches[0];
+}, false);
+document.addEventListener('touchend', function(event) {
+event.preventDefault();
+event.stopPropagation();
+finalPoint = event.changedTouches[0];
+var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+if (xAbs > 20 || yAbs > 20) {
+if (xAbs > yAbs) {
+if (finalPoint.pageX < initialPoint.pageX){
+    intervalCounter++;
+    if (intervalCounter > len) intervalCounter = 1;
+    $(".slider").attr("data-slider", intervalCounter);
+    $(`.slide_info-wrapper`).removeClass('slide_info-wrapper_active')
+    $(`.slide_info-wrapper[data-content="${intervalCounter}"]`).addClass('slide_info-wrapper_active')
+    $(`input[type='radio']`)[intervalCounter - 1].checked = true;
+}
+else {
+    intervalCounter--;
+    if (intervalCounter < len) intervalCounter = 3;
+    $(".slider").attr("data-slider", intervalCounter);
+    $(`.slide_info-wrapper`).removeClass('slide_info-wrapper_active')
+    $(`.slide_info-wrapper[data-content="${intervalCounter}"]`).addClass('slide_info-wrapper_active')
+    $(`input[type='radio']`)[intervalCounter - 1].checked = true;
+}
+}}}, false);
