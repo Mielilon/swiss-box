e8432f8cb83783c59882b82f5de1d9c4de1ad912
Mielilon <daria.mg354@mail.ru>
2020-11-13 13:20:09 +0300

    Сделан десктопный вариант

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 //= jquery.min.js
+
+$(`input[type='radio']`).change(function() {
+    const id = $(this).val()
+    $(".slider").attr("data-slider", id);
+    $(`.slide_info-wrapper`).removeClass('slide_info-wrapper_active')
+    $(`.slide_info-wrapper[data-content="${id}"]`).addClass('slide_info-wrapper_active')
+});
